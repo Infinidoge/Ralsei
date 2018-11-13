c0016bc8c0065c3575e83602a864f3d0171e19fd
Infinidoge <sten.erickson04@gmail.com>
2018-11-13 14:26:48 -0500

    Alias System Initial Creation

--- ralsei.py
+++ ralsei.py
@@ -7,13 +7,19 @@
 
 import discord
 import asyncio
+import utils.alias_sys as alias_mod
 from utils.misc_utils import find_files
 from utils.config import Client
 from utils.permissions import Perms
 
+
 client = discord.Client()
 config = Client()
 perms = Perms()
+
+alias = alias_mod.Alias()
+
+
 if perms.owner_id == "blank":
     print("[Ralsei/Warn] owner id not set, continuing to run bot but beware that some commands may be unusable")
 
@@ -36,9 +42,12 @@ async def exec_cmd(message):
     try:
         await cmd[message.content.replace(config.prefix, "").split(' ')[0]](client, message)
     except KeyError:
-        await client.send_message(message.channel,
-                                  "I'm sorry <@%s>, but i have no idea what you just asked me to do. Sorry!" %
-                                  message.author.id)
+        try:
+            alias(message.content.replace(config.prefix, "").split(' ')[0])
+        except KeyError:
+            await client.send_message(message.channel,
+                                      "I'm sorry <@%s>, but i have no idea what you just asked me to do. Sorry!" %
+                                      message.author.id)
 
 
 async def post_cmd(message):
@@ -57,6 +66,8 @@ for i in find_files(config.location + "cmds"):
     i = i.replace(".py", "")
     exec("from cmds.%s import %s" % (i, i), globals())
     cmd[i.replace("_cmd", "")] = eval(i)
+
+
 
 
 @client.event
---
+++ utils/alias_sys.py
@@ -0,0 +1,24 @@
+# ----------------------------------
+# Ralsei/utils/alias_sys
+# Created by Infinidoge
+# ----------------------------------
+# Utility base for creating aliases for commands
+# ----------------------------------
+
+from cmds.roll_cmd import roll_cmd
+from utils.permissions import Perms
+perms = Perms()
+
+
+class Alias:
+    async def d20(self, client):
+        await roll_cmd(client, "!roll 1d20")
+
+    def __init__(self):
+        self.alias = {"d20": self.d20}
+
+    def __call__(self, cmd):
+        try:
+            self.alias[cmd]
+        except KeyError:
+            raise KeyError
