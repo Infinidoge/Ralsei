f1e25b65545aa7bea3fd29581a2b1d168e748365
Infinidoge <sten.erickson04@gmail.com>
2018-11-14 13:14:34 -0500

    Fixed Roll cmd

--- cmds/roll_cmd.py
+++ cmds/roll_cmd.py
@@ -9,6 +9,8 @@
 import random
 import discord
 
+dice_max = 500
+sides_max = 950
 
 async def roll_cmd(client, message):
     cmd = message.content[6:]
@@ -18,16 +20,17 @@ async def roll_cmd(client, message):
         pre = cmd.split("+")
         dice, mod = pre
         dice = dice.lower().split("d")
-        if int(dice[0]) > 500 or int(dice[1]) > 1000:
+        if int(dice[0]) > dice_max or int(dice[1]) > sides_max:
             await client.send_message(message.channel,
-                                      "Those numbers are a little too large for my handful of dice, sorry!")
+                                      "Those numbers are a little too large for my handful of dice, sorry!\n" +
+                                      "the limit is 500 dice with 950 sides each, with no maximum for the modifier.")
             return
         rolls = []
         try:
             for i in range(int(dice[0])):
                 roll = random.randint(1, int(dice[1]))
                 rolls.append(roll)
-            await client.send_message(message.channel, "[<@%s>]\n:game_die: Rolling: %sd%s:\n%s\nsum:%s" %
+            await client.send_message(message.channel, "[<@%s>]\n:game_die: **Rolling:** %sd%s:\n%s\n**sum:** %s" %
                                       (message.author.id,
                                        str(dice[0]),
                                        str(dice[1]),
@@ -38,22 +41,23 @@ async def roll_cmd(client, message):
                                        str(sum(rolls) + int(mod))))
         except discord.errors.HTTPException:
             await client.send_message(message.channel,
-                                      "Those numbers are a little too large for my handful of dice, sorry!")
+                                      "Those numbers are a little too large for discord to handle, sorry!")
 
     elif "-" in cmd:
         pre = cmd.split("-")
         dice, mod = pre
         dice = dice.lower().split("d")
-        if int(dice[0]) > 500 or int(dice[1]) > 1000:
+        if int(dice[0]) > dice_max or int(dice[1]) > sides_max:
             await client.send_message(message.channel,
-                                      "Those numbers are a little too large for my handful of dice, sorry!")
+                                      "Those numbers are a little too large for my handful of dice, sorry!\n" +
+                                      "the limit is 500 dice with 950 sides each, with no maximum for the modifier.")
             return
         try:
             rolls = []
             for i in range(int(dice[0])):
                 roll = random.randint(1, int(dice[1]))
                 rolls.append(roll)
-            await client.send_message(message.channel, "[<@%s>]\n:game_die: Rolling: %sd%s:\n%s\nsum:%s" %
+            await client.send_message(message.channel, "[<@%s>]\n:game_die: **Rolling:** %sd%s:\n%s\n**sum:** %s" %
                                       (message.author.id,
                                        str(dice[0]),
                                        str(dice[1]),
@@ -68,16 +72,18 @@ async def roll_cmd(client, message):
 
     else:
         cmd = cmd.lower().split("d")
-        if int(cmd[0]) > 500 or int(cmd[1]) > 1000:
+        if int(cmd[0]) > dice_max or int(cmd[1]) > sides_max:
             await client.send_message(message.channel,
-                                      "Those numbers are a little too large for my handful of dice, sorry!")
+                                      "Those numbers are a little too large for my handful of dice, sorry!\n" +
+                                      "the limit is 500 dice with 950 sides each, with no maximum for the modifier.")
+
             return
         try:
             rolls = []
             for i in range(int(cmd[0])):
                 roll = random.randint(1, int(cmd[1]))
                 rolls.append(roll)
-            await client.send_message(message.channel, "[<@%s>]\n:game_die: Rolling: %sd%s:\n%s\nsum:%s" %
+            await client.send_message(message.channel, "[<@%s>]\n:game_die: **Rolling:** %sd%s:\n%s\n**sum:** %s" %
                                       (message.author.id,
                                        str(cmd[0]),
                                        str(cmd[1]),
@@ -88,4 +94,4 @@ async def roll_cmd(client, message):
 
         except discord.errors.HTTPException:
             await client.send_message(message.channel,
-                                      "Those numbers are a little too large for my handful of dice, sorry!")
+                                      "Those numbers are a little too large for discord to handle, sorry!")
